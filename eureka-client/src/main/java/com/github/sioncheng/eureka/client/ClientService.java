1478fd4b5e8550a2cc5d12a74a0c3dd36f942833
sion <sion@SionMacBookPro.local>
2019-08-18 17:02:55 +0800

    eureka server,servie,consumer.

--- eureka-client/src/main/java/com/github/sioncheng/eureka/client/ClientService.java
+++ eureka-client/src/main/java/com/github/sioncheng/eureka/client/ClientService.java
@@ -4,14 +4,16 @@ package com.github.sioncheng.eureka.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableDiscoveryClient
 public class ClientService {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientService.class);
+        AutoServiceRegistrationConfiguration a;
     }
 }
--- eureka-customer-client/src/main/java/com/github/sioncheng/ecc/ConsumerCommandLineRunner.java
+++ eureka-customer-client/src/main/java/com/github/sioncheng/ecc/ConsumerCommandLineRunner.java
@@ -3,21 +3,70 @@ package com.github.sioncheng.ecc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
+@Configuration
 public class ConsumerCommandLineRunner implements CommandLineRunner {
 
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private List<RestTemplate> restTemplates = Collections.emptyList();
+
+    @Autowired
+    private List<RestTemplate> restTemplates2;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println(discoveryClient == null);
 
-        discoveryClient.getInstances("CLIENT-SERVICE").forEach(x -> {
-            System.out.println(x.getHost() + ":" + x.getPort());
-        });
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("CLIENT-SERVICE");
+
+        System.out.println(serviceInstanceList.isEmpty());
+        serviceInstanceList.forEach(x -> System.out.println(x.getHost() + ":" + x.getPort()));
+
+
+        String res = restTemplate.getForObject("http://client-service/svr1/hello", String.class);
+        System.out.println(res);
+
+        System.out.println(restTemplates.isEmpty());
+
+        if (!restTemplates.isEmpty()) {
+            restTemplates.forEach(x -> System.out.println(x == restTemplate));
+        }
+
+        System.out.println(restTemplates2.size());
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public List<RestTemplate> restTemplatesBuilder() {
+        return Arrays.asList(new RestTemplate(), new RestTemplate());
+    }
+
+    @Bean
+    public RestTemplate restTemplate2() {
+        return new RestTemplate();
     }
 }
